0707f60cde20d302c9d4510952239b2a8dfc5b4e
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-28 18:10:16 +0000

    class created and functions

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
+class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement=previousOperandTextElement
+        this.currentOperandTextElement=currentOperandTextElement
+    }
+    clear(){
+
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+
+    }
+    chooseOperation(opearation){
+
+    }
+    compute(){
+
+    }
+    updateDisplay(){
+
+    }
+
+}
+
+
 const numberButtons=document.querySelector('[data-number]')//to select all the elements that have data number
 const operationButtons=document.querySelector('[data-operation]')//to select all the elements that opearation button
+const equalButton=document.querySelector('[data-equals]')
+const deleteButton=document.querySelector('[data-delete]')
+const allClearButton=document.querySelector('[data-all-clear]')
+const previousOperandTextElement=document.querySelector('[data-previous-operand]')
+const currentOperandTextElement=document.querySelector('[data-current-operand]')
